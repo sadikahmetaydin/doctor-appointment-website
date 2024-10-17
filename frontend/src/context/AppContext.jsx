9bba0e503eb1d5f74bba061dc0341ebb9ad8b763
the-pro7 <ameyawemmanuel900@gmail.com>
2024-10-17 16:22:54 +0000

    first touch to repo

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -3,21 +3,15 @@ import { doctors } from "../assets/assets_frontend/assets";
 
 export const AppContext = createContext();
 
-const AppContextProvider = (props) => {
-
-  const currencySymbol = '$'
+const AppContextProvider = ({ children }) => {
+  const currencySymbol = "$";
 
   const value = {
-    doctors, 
-    currencySymbol
-  }
+    doctors,
+    currencySymbol,
+  };
 
-  return (
-    <AppContext.Provider value={value}>
-      {props.children}
-    </AppContext.Provider>
-  )
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
 
-}
-
-export default AppContextProvider
+export default AppContextProvider;
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { assets } from '../assets/assets_frontend/assets'
+import AboutImage from '../assets/assets_frontend/about_image.png'
 
 const About = () => {
   return (
@@ -9,7 +9,7 @@ const About = () => {
       </div>
 
       <div className='my-10 flex flex-col md:flex-row gap-12'>
-        <img className='w-full md:max-w-[360px]' src={assets.about_image} alt="" />
+        <img className='w-full md:max-w-[360px]' src={AboutImage} alt="Two doctors standing and smiling" />
 
         <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
           <p>Welcome to Prescripto, your trusted partner in managing your healthcare needs conveniently and efficiently. At Prescripto, we understand the challenges individuals face when it comes to scheduling doctor appointments and managing their health records.</p>
